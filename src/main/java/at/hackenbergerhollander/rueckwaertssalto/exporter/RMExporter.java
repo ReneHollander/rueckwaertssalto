c4f5d0bc2a43be94544305d886577245c9c7ce2a
Christoph Hackenberger <chackenberger@student.tgm.ac.at>
2015-01-21 09:33:53 +0100

    Pushing shit to production

--- src/main/java/at/hackenbergerhollander/rueckwaertssalto/exporter/RMExporter.java
+++ src/main/java/at/hackenbergerhollander/rueckwaertssalto/exporter/RMExporter.java
@@ -1,13 +1,32 @@
 package at.hackenbergerhollander.rueckwaertssalto.exporter;
 
+import at.hackenbergerhollander.rueckwaertssalto.dbstructure.Attribute;
 import at.hackenbergerhollander.rueckwaertssalto.dbstructure.Database;
+import at.hackenbergerhollander.rueckwaertssalto.dbstructure.Table;
+import com.tutego.jrtf.Rtf;
+import com.tutego.jrtf.RtfText;
+
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+import static com.tutego.jrtf.Rtf.rtf;
+import static com.tutego.jrtf.RtfDocfmt.*;
+import static com.tutego.jrtf.RtfHeader.*;
+import static com.tutego.jrtf.RtfInfo.*;
+import static com.tutego.jrtf.RtfFields.*;
+import static com.tutego.jrtf.RtfPara.*;
+import static com.tutego.jrtf.RtfSectionFormatAndHeaderFooter.*;
+import static com.tutego.jrtf.RtfText.*;
+import static com.tutego.jrtf.RtfUnit.*;
 
 /**
  * Exporter for a relational model
  *
- * @author Hackenberger Christoph
+ * @author Hackenberger Christoph chackenberger@student.tgm.ac.at
  * @version 1.0
  */
 public class RMExporter implements Exporter {
@@ -29,6 +48,24 @@ public class RMExporter implements Exporter {
      */
     @Override
     public boolean export(Database db) {
-        return false;
+
+        Table[] tables = db.getTables();
+        List<RtfText> lines = new ArrayList<>();
+        for(Table table : tables) {
+            Attribute[] attributes = table.getAttributes();
+            for(Attribute attribute : attributes) {
+                Map<String, Object> props = attribute.getProperties();
+                RtfText text;
+                if(props.containsKey(Attribute.PROPERTY_PRIMARY_KEY)) {
+                    text = RtfText.underline(text);
+                    if(props.containsKey(Attribute.PROPERTY_FOREIGN_KEY)) {
+                        text = RtfText.dottedUnderline(underline(attribute.getProperties().get(Attribute.PROPERTY_FOREIGN_KEY)));
+                    }
+                } else {
+                    text = RtfText.text(attribute.getName());
+                }
+                lines.add(text);
+            }
+        }
     }
 }
